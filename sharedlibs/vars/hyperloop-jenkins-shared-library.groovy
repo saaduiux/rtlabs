3d997a499f8c5501c91792d4b01949e3e3b0af35
saaduiux <94008374+saaduiux@users.noreply.github.com>
2023-01-26 19:10:02 +0000

    adding sharedlibs

--- sharedlibs/vars/hyperloop-jenkins-shared-library.groovy
+++ sharedlibs/vars/hyperloop-jenkins-shared-library.groovy
@@ -1,5 +1,9 @@
+// def changelog() {
+//     dir("changelog/")
+//     sh 'pip3 install -r requirements'
+//     sh 'python3 docker_changelog.py --report ${JENKINS_HOME}/jobs/${JOB_NAME}/build/${BUILD_NUMBER}/archive/qualys_images_summary.json --image ${JOB_NAME}'
+// }
+
 def changelog() {
-    dir("changelog/")
-    sh 'pip3 install -r requirements'
-    sh 'python3 docker_changelog.py --report ${JENKINS_HOME}/jobs/${JOB_NAME}/build/${BUILD_NUMBER}/archive/qualys_images_summary.json --image ${JOB_NAME}'
+    echo "this is ${BUILD_NUMBER}"
 }
